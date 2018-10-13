17b00fc8911486afac3f2f6878017ac9fbc0a687
csaborio <christian@saborio.org>
2018-10-14 10:05:17 +1100

    Version 1.0

--- README.md
+++ README.md
@@ -1,18 +1,53 @@
 # scorpiotek-wp-util
+---
 Classes for common WordPress tasks such as logging, modifying admin menu items, etc.
 
-For modifying admin menus activate the pluging and use the following code (ideally in your functions.php file):
+# Menu Utilities
 
-use \ScorpioTek\WordPress\Util\Menu\AdminMenuModifier as AdminMenuModifier;
+The class AdminMenuModifier provides an easy way to modify admin menus. Namely, it 
+makes it possible to remove any admin menu for any particular user.
 
-function clean_admin_menu() {
-    // Menu Modifications
-    if ( class_exists ('ScorpioTek\WordPress\Util\Menu\AdminMenuModifier') ) {
-        AdminMenuModifier::remove_posts_menu( 'editor' );
+## Usage
+
+* Activate the plugin as with any normal plugin. You won't see anything but you will now be able to use
+the class.
+
+* Use the following statement to easily invoke the class:
+
+        use \ScorpioTek\WordPress\Util\Menu\AdminMenuModifier as AdminMenuModifier;
+
+* Attach the function that will modify the menus and call the respective function:
+
+        if ( class_exists ('ScorpioTek\WordPress\Util\Menu\AdminMenuModifier') ) {
+        $cesaa_AdminMenuModifier = new AdminMenuModifier();
+
+* You can now invoke any of the functions and specify which users the function will affect. For instance, to remove the 
+WP logo for editor users, you invoke the function as follows:
+
+        $cesaa_AdminMenuModifier->remove_item_from_wp_adminbar( 'wp_admin_bar_logo', array( 'editor' ) );
+
+The *remove_menu_from_admin_sidebar* and *remove_item_from_wp_adminbar* expect a menu_name parameter that defines
+what will be removed. These values are defined by the labels inside the menu_option_ids array inside the AdminMenuModifier class:
+
+    private $menu_option_ids = array (
+        'admin_menu_main_posts' => 'edit.php',
+        'admin_menu_main_pages' => 'edit.php?post_type=page',
+        'admin_menu_main_comments' => 'edit-comments.php',
+        'admin_menu_main_tools' => 'tools.php',
+        'admin_menu_main_media' => 'upload.php',
+        'admin_menu_main_envira' => 'edit.php?post_type=envira',
+        'admin_menu_main_gravity_forms' => 'gf_entries',
         ...
-    }
-}
-add_action('admin_menu', 'clean_admin_menu');
+
+
+## Version History 
+
+1.0 - October 13, 2018
+
+* Initial Release
+* Refactored AdminMenuModifier code to use less functions and allow the user to define items to be removed by using parameters
+* Documented the code in the AdminMenuModifier class
+
 
 
 
--- include/scorpiotek-menu-util.php
+++ include/scorpiotek-menu-util.php
@@ -3,95 +3,128 @@
 namespace ScorpioTek\WordPress\Util\Menu;
 
 class AdminMenuModifier {
-
-    public static function remove_posts_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'edit.php' );
-        }
-    }
-
-    public static function remove_pages_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'edit.php?post_type=page' );
-        }
-    }    
-
-    public static function remove_comments_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'edit-comments.php' );
-        }
-    }    
-
-    public static function remove_tools_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'tools.php' );
-        }
-    }    
-
-    public static function remove_media_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'upload.php' );
-        }
-    }    
-
-    public static function remove_envira_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'edit.php?post_type=envira' );
-        }
-    }    
-
-    public static function remove_forms_menu ( $capability ) {
-        if ( current_user_can( $capability ) ) {
-            remove_menu_page( 'admin.php?page=gf_entries' );
-        }
-    } 
     
-    public static function remove_new_from_admin_bar( $capability ) {
-        add_action( 'wp_before_admin_bar_render', function() use ( $capability ) {
-            if ( current_user_can( $capability ) ) {
-                global $wp_admin_bar;
-                // die(var_dump($wp_admin_bar));
-                $wp_admin_bar->remove_menu('new-content');
+    /* 
+     * These are the allowed operations contained into in an array
+     * When remove_item_from_wpadminbar or remove_menu_from_admin_sidebar is invoked
+     * we check to see if the menu name exists in this array
+     */
+    private $menu_option_ids = array (
+        'admin_menu_main_posts' => 'edit.php',
+        'admin_menu_main_pages' => 'edit.php?post_type=page',
+        'admin_menu_main_comments' => 'edit-comments.php',
+        'admin_menu_main_tools' => 'tools.php',
+        'admin_menu_main_media' => 'upload.php',
+        'admin_menu_main_envira' => 'edit.php?post_type=envira',
+        'admin_menu_main_gravity_forms' => 'gf_entries',
+        'wp_admin_bar_plus_menu' => 'new-content',
+        'wp_admin_bar_sidebar_comments' => 'comments',
+        'wp_admin_bar_logo' => 'wp-logo',
+        'wp_admin_bar_view_posts' => 'archive'
+    );
+
+/**
+  * @summary Removes elements from the admin sidebar for one or more capabilities
+  *
+  * @description Some elements in the admin sidebar do not need to be displayed for certain users 
+  * or groups. This function removes the elements specified for those user groups
+  *
+  * @author Christian Saborio <csaborio@scorpiotek.com>
+  *
+  * @param string $menu_name the menu id to be removed. Has to match one of the keys available in $menu_option_ids
+  * @param array $capabilities an array that specifies one ore more capability to remove the menu item for
+  * @return void
+  */
+  
+    public function remove_menu_from_admin_sidebar ( $menu_name, $capabilities ) {
+        if ( array_key_exists( $menu_name, $this->get_menu_option_ids() ) ) {
+            if ( is_array( $capabilities) ) {
+                foreach( $capabilities as $capability ) {
+                    if ( current_user_can( $capability ) ) {
+                        remove_menu_page( $this->get_menu_option_ids()[$menu_name] );
+                    }
+                }
             }
-        });
+        }
     }
-    
-    public static function remove_comment_from_admin_bar( $capability ) {
-        add_action( 'wp_before_admin_bar_render', function() use ( $capability ) {
-            if ( current_user_can( $capability ) ) {
-                global $wp_admin_bar;
-                $wp_admin_bar->remove_menu('comments');
+ 
+/**
+  * @summary Removes elements from the wp admin bar (the horizonal one)
+  *
+  * @description Some elements in the wp admin bar sidebar do not need to be displayed for certain users 
+  * or groups. This function removes the elements specified for those user groups
+  *
+  * @author Christian Saborio <csaborio@scorpiotek.com>
+  *
+  * @param string $menu_name the menu id to be removed. Has to match one of the keys available in $menu_option_ids
+  * @param array $capabilities an array that specifies one ore more capability to remove the menu item for
+  * @return void
+  *
+  */
+  
+    public function remove_item_from_wp_adminbar( $menu_name, $capabilities ) {
+        add_action( 'wp_before_admin_bar_render', function() use ( $menu_name, $capabilities ) {
+            if ( array_key_exists( $menu_name, $this->get_menu_option_ids() ) ) {
+                if ( is_array( $capabilities) ) {
+                    foreach( $capabilities as $capability ) {
+                            global $wp_admin_bar;
+                            $wp_admin_bar->remove_menu($this->get_menu_option_ids()[$menu_name]);
+                    }
+                }
             }
-        });
+        });        
     }
-    
-    public static function remove_wordpress_logo_from_admin_bar( $capability ) {
-        add_action( 'wp_before_admin_bar_render', function() use ( $capability ) {
-            if ( current_user_can( $capability ) ) {
-                global $wp_admin_bar;
-                $wp_admin_bar->remove_menu('wp-logo');
-            }
-        });
-    }
-    
-    public static function replace_howdy_from_admin_bar( $new_greeting, $capability ) {
-        add_action( 'wp_before_admin_bar_render', function( ) use ( $new_greeting, $capability ) {
-            if ( current_user_can( $capability ) ) {
-                global $wp_admin_bar;
-                $my_account = $wp_admin_bar->get_node('my-account');
-                $new_title = str_replace( 'Howdy', $new_greeting, $my_account->title );
-                $wp_admin_bar->add_node( array(
-                        'id' => 'my-account',
-                        'title' => $new_title
-                    )
-                );
+
+/**
+  * @summary Replaces the default 'Howdy ' greeting with a new greeting
+  *
+  * @description WordPress adds 'Howdy ' before the username in the top right corner of the backed.
+  * In some scenarios the 'Howdy' greeting is not appropriate. This function allows the greeting
+  * to be changed for one or more capabilities.
+  *
+  * @author Christian Saborio <csaborio@scorpiotek.com>
+  *
+  * @param string $new_greeting the text that will replace 'Howdy'
+  * @param array $capabilities the array that contains the capabilities to which to apply the replacement
+  *
+  * @return void
+  */
+  
+    public function replace_howdy_from_admin_bar( $new_greeting, $capabilities ) {
+        add_action( 'wp_before_admin_bar_render', function( ) use ( $new_greeting, $capabilities ) {
+            if ( is_array( $capabilities) ) {
+                foreach( $capabilities as $capability ) {            
+                    if ( current_user_can( $capability ) ) {
+                        global $wp_admin_bar;
+                        $my_account = $wp_admin_bar->get_node('my-account');
+                        $new_title = str_replace( 'Howdy', $new_greeting, $my_account->title );
+                        $wp_admin_bar->add_node( array(
+                                'id' => 'my-account',
+                                'title' => $new_title
+                            )
+                        );
+                    }
+                }
             }
         });
     }
     
     // http://bit.ly/2xdzD07
 
-    public static function add_menu_separator( $position ) {
+/**
+  * @summary Adds a WordPress separator (some space) after a particular menu
+  *
+  * @description WordPress allows the use of separators to give a feeling of grouping between certain elements 
+  * This function allows to easily add these separators.
+  *
+  * @author Christian Saborio <csaborio@scorpiotek.com>
+  *
+  * @param int $position the position in which to insert the separator in the WordPress menu
+  *
+  * @return void
+  */
+  
+    public function add_menu_separator( $position ) {
         add_action( 'admin_init', function() use ( $position ) {
             global $menu;
             $menu[ $position ] = array(
@@ -105,4 +138,38 @@ class AdminMenuModifier {
 
     }
 
+/**
+  * @summary Simple Getter for the Menu Option IDs of this class
+  *
+  * @description description
+  *
+  * @author Christian Saborio <csaborio@scorpiotek.com>
+  *
+  * @return array A copy of the $menu_option_ids array
+  */
+  
+
+    public function get_menu_option_ids() {
+            return $this->menu_option_ids;
+    }
+
+     /**
+     * @summary Prints out the menu hierarchy in Array Notation, useful for debugging. Do not use in production.
+    *
+    * @description The menu admin sidebar structure in WordPress is created from an array. 
+    * Each menu item contains arrays that contain properties for each of the elements. This function will print those values.
+    * The user can then use 'View Source' to get a good idea of the array structure. 
+    * Very useful if the user wants to find the ID of an element to be modified/deleted
+    *
+    * @author Christian Saborio <csaborio@scorpiotek.com>
+    *
+    */
+  
+    public function debug_admin_menu() {
+        if ( WP_DEBUG ) {
+            add_action( 'admin_init',  function() {
+                echo '<pre>' . print_r( $GLOBALS[ 'menu' ], TRUE) . '</pre>';
+            });
+        }
+    }
 }
